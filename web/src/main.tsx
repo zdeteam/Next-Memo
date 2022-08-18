bf9802040e54f4dd60ff243f3ad6b970f31e308a
Aha <yowangbin@gmail.com>
2022-08-19 07:19:58 +0800

    update trash style

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from "@strapi/design-system/ThemeProvider";
 import { lightTheme } from "@strapi/design-system/themes";
 import store from "./store";
 import "./helpers/polyfill";
+import "./less/global.less";
 import "./css/index.css";
 
 const container = document.getElementById("root");
