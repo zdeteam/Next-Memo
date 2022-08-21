3139b45ece3d3b6ebd77aa19ace3a4628469cdd1
Aha <yowangbin@gmail.com>
2022-08-21 13:07:34 +0800

    update daily review style

--- web/src/components/DailyReviewDialog.tsx
+++ web/src/components/DailyReviewDialog.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 import { useAppSelector } from "../store";
 import toImage from "../labs/html2image";
 import useToggle from "../hooks/useToggle";
@@ -8,7 +9,6 @@ import { generateDialog } from "./Dialog";
 import DatePicker from "./common/DatePicker";
 import Button from "./common/Button";
 import DailyMemo from "./DailyMemo";
-import ProseMirrorEditor from "./Editor/ProseMirrorEditor";
 import "../less/daily-review-dialog.less";
 
 interface Props extends DialogProps {
@@ -19,8 +19,9 @@ const monthChineseStrArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "
 const weekdayChineseStrArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const DailyReviewDialog: React.FC<Props> = (props: Props) => {
+  const todayStamp = utils.getDateStampByDate(utils.getDateString(props.currentDateStamp));
   const memos = useAppSelector((state) => state.memo.memos);
-  const [currentDateStamp, setCurrentDateStamp] = useState(utils.getDateStampByDate(utils.getDateString(props.currentDateStamp)));
+  const [currentDateStamp, setCurrentDateStamp] = useState(todayStamp);
   const [showDatePicker, toggleShowDatePicker] = useToggle(false);
   const memosElRef = useRef<HTMLDivElement>(null);
   const currentDate = new Date(currentDateStamp);
@@ -60,31 +61,41 @@ const DailyReviewDialog: React.FC<Props> = (props: Props) => {
 
   return (
     <div className="daily-review-wrapper">
-      <div className="content" ref={memosElRef}>
-        <div className="date-card-container">
-          {showDatePicker && (
-            <DatePicker className={`date-picker`} datestamp={currentDateStamp} handleDateStampChange={handleDataPickerChange} />
-          )}
-          <div className="year-text">{currentDate.getFullYear()}</div>
-          <div className="date-container" onClick={() => toggleShowDatePicker()}>
-            <div className="month-text">{monthChineseStrArray[currentDate.getMonth()]}</div>
-            <div className="date-text">{currentDate.getDate()}</div>
-            <div className="day-text">{weekdayChineseStrArray[currentDate.getDay()]}</div>
+      {showDatePicker ? (
+        <>
+          <DatePicker className={`date-picker`} datestamp={currentDateStamp} handleDateStampChange={handleDataPickerChange} />
+          <Button onClick={() => handleDataPickerChange(todayStamp)}>回到今天</Button>
+        </>
+      ) : (
+        <>
+          <div className="day-change">
+            <GoChevronLeft onClick={() => setCurrentDateStamp(currentDateStamp - DAILY_TIMESTAMP)} />
+            <GoChevronRight onClick={() => setCurrentDateStamp(currentDateStamp + DAILY_TIMESTAMP)} />
           </div>
-        </div>
-        {dailyMemos.length === 0 ? (
-          <div className="tip-container">
-            <p className="tip-text">Oops, there is nothing.</p>
+          <div className="content" ref={memosElRef}>
+            <div className="date-card-container" onClick={() => toggleShowDatePicker()}>
+              <div className="year-text">{currentDate.getFullYear()}</div>
+              <div className="date-container">
+                <div className="month-text">{monthChineseStrArray[currentDate.getMonth()]}</div>
+                <div className="date-text">{currentDate.getDate()}</div>
+                <div className="day-text">{weekdayChineseStrArray[currentDate.getDay()]}</div>
+              </div>
+            </div>
+            {dailyMemos.length === 0 ? (
+              <div className="tip-container">
+                <p className="tip-text">今天休息啦</p>
+              </div>
+            ) : (
+              <div className="dailymemos-wrapper">
+                {dailyMemos.map((memo) => (
+                  <DailyMemo key={`${memo.id}-${memo.updatedTs}`} memo={memo} />
+                ))}
+              </div>
+            )}
           </div>
-        ) : (
-          <div className="dailymemos-wrapper">
-            {dailyMemos.map((memo) => (
-              <DailyMemo key={`${memo.id}-${memo.updatedTs}`} memo={memo} />
-            ))}
-          </div>
-        )}
-      </div>
-      <Button onClick={handleShareBtnClick}>分享卡片</Button>
+          {dailyMemos.length !== 0 && <Button onClick={handleShareBtnClick}>分享卡片</Button>}
+        </>
+      )}
     </div>
   );
 };
