14d6b7d9b375a3decd960d34bb0951421e9a4e43
Aha <yowangbin@gmail.com>
2022-08-22 17:21:47 +0800

    add modal component

--- web/src/components/common/Modal.tsx
+++ web/src/components/common/Modal.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { GoX } from "react-icons/go";
 import "../../less/common/modal.less";
+
 interface ModalProps {
   children?: any;
+  onClose?: () => void;
+  closeable?: boolean;
   visible: boolean;
 }
 
 const Modal = (props: ModalProps) => {
-  if (!props.visible) return null;
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    console.log(111);
+    setVisible(props.visible);
+  }, [props.visible]);
+
+  if (!visible) return null;
+
   return (
-    <div className="modal">
-      <div>{props.children}</div>
+    <div className="modal-wrapper">
+      <div>
+        {props.closeable && (
+          <div className="header">
+            <GoX
+              onClick={() => {
+                props.onClose && props.onClose();
+              }}
+            />
+          </div>
+        )}
+        <div className="modal">{props.children}</div>
+      </div>
     </div>
   );
 };
